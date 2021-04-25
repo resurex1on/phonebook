03dd4b702e875d410097019be6aac12c81be3a98
lo7656 <vladleonchik@mail.ru>
2021-04-26 00:38:52 +0300

    add jsx and scss files to search

--- app/app/src/App.js
+++ app/app/src/App.js
@@ -1,12 +1,14 @@
+import React from 'react';
 import './App.css';
+import Search from './modules/search/search'
 
 function App() {
   return (
     <div>
-      
-      
+
+      <Search />
     </div>
-    
+
   )
 }
 
---
+++ app/app/src/modules/search/search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function Search() {
+  return(
+    <section>
+
+    </section>
+  )
+}
+
+export default Search
