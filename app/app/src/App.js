ffb16b6ae0ea6899d3d90eea9dda0230efdb4e31
lo7656 <vladleonchik@mail.ru>
2021-04-25 10:41:44 +0300

    create some files for nav

--- app/app/src/App.js
+++ app/app/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './modules/nav/nav'
 
 function App() {
   return (
     <div>
+      <Navbar/>
     </div>
     
   )
---
+++ app/app/src/modules/nav/nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './nav.scss'
+
+function Navbar() {
+  return (
+    <nav>
+      <h1 className="hi">hi</h1>
+    </nav>
+  )
+}
+
+export default Navbar
