36ce4a14bf2f6ca02b0e7e581af07937c5dfd5c9
lo7656 <vladleonchik@mail.ru>
2021-04-25 12:55:03 +0300

    add search input

--- app/app/src/modules/nav/nav.jsx
+++ app/app/src/modules/nav/nav.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './nav.scss'
+import {InputGroup} from 'react-bootstrap'
+import {FormControl} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function Navbar() {
   return (
-    <nav>
-      <h1 className="hi">hi</h1>
+    <nav className="navbar">
+      <h1 className="navbar-logo">Phonebook</h1>
+      <div className="navbar__tools">
+      <div class="input-group">
+  <div class="form-outline">
+    <input type="search" id="form1" class="form-control" />
+  </div>
+  <button type="button" class="btn btn-primary">
+    <i class="fas fa-search">Search</i>
+  </button>
+</div>
+      </div>
+      
     </nav>
   )
 }
