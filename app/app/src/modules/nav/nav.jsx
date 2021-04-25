035d7ed452be38a0e4c904c321a797f3b5ee6974
lo7656 <vladleonchik@mail.ru>
2021-04-26 00:24:10 +0300

    add some styles and markup to nav

--- app/app/src/modules/nav/nav.jsx
+++ app/app/src/modules/nav/nav.jsx
@@ -7,16 +7,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1 className="navbar-logo">Phonebook</h1>
-      <div className="navbar__tools">
-      <div class="input-group">
-  <div class="form-outline">
-    <input type="search" id="form1" class="form-control" />
-  </div>
-  <button type="button" class="btn btn-primary">
-    <i class="fas fa-search">Search</i>
-  </button>
-</div>
+      <div className="contain navbar__contain">
+      <h1 className="navbar-logo">Phone<span className="navbar-book">book</span></h1>
+      <ul className="navbar__tools">
+        {/* <div class="input-group">
+          <input type="search" id="form1" class="form-control" />
+          <button type="button" class="btn btn-primary">
+            <i class="fas fa-search">Поиск</i>
+          </button>
+        </div> */}
+        
+        <li className="navbar__tools-item"><a className="navbar__tools-link" href="#">Руководство</a></li>
+        <li className="navbar__tools-item"><a className="navbar__tools-link" href="#">Обратная связь</a></li>
+        <li className="navbar__tools-item"><button type="button" class="btn btn-primary">Войти</button></li>
+        
+      </ul>
+      
       </div>
       
     </nav>
