695c362b64128e6a03673df0e9daa1cd557a1c67
lo7656 <vladleonchik@mail.ru>
2021-04-26 08:22:34 +0300

    add result-block

--- app/app/src/modules/search/search.jsx
+++ app/app/src/modules/search/search.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './search.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {ListGroup} from 'react-bootstrap'
+import './search.scss'
 
 function Search() {
   return(
-    <section>
-
+    <section className="search">
+      <div className="contain">
+        <ListGroup className="search__results">
+        <ListGroup.Item className="search__results__result">
+            <p className="search__results__result-name">Номер</p>
+            <p className="search__results__result-name">Адрес</p>
+            <p className="search__results__result-name">Название</p>
+        </ListGroup.Item>
+        <ListGroup.Item className="search__results__result">
+          <p className="search__results__result-property">+375333949750</p>
+          <p className="search__results__result-property">Рокоссовского 53 кв 50</p>
+          <p className="search__results__result-property">Леончик ВЮ</p>  
+        </ListGroup.Item>
+      </ListGroup>
+      </div>
+      
     </section>
   )
 }
