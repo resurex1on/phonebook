d3dfb45042f711829c842b24371303ce28697ff5
lo7656 <vladleonchik@mail.ru>
2021-04-27 03:48:56 +0300

    add search-block

--- app/app/src/modules/search/search.jsx
+++ app/app/src/modules/search/search.jsx
@@ -1,12 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {ListGroup} from 'react-bootstrap'
+import {InputGroup} from 'react-bootstrap'
+import {FormControl} from 'react-bootstrap'
 import './search.scss'
 
 function Search() {
   return(
     <section className="search">
-      <div className="contain">
+      <div className="contain search__contain">
+        <p className="search__info-name">Поиск по телефонной книге</p>
+        <div className="search__info">
+          <div className="search__info-property">
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">Номер</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+              className="mb-3-input"
+              placeholder="+375XXXXXXXX"
+              aria-label="Username"
+              aria-describedby="basic-addon1"/>
+            </InputGroup>
+          </div>
+           <div className="search__info-property">
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">Адрес</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+              className="mb-3-input"
+              placeholder="+375XXXXXXXX"
+              aria-label="Username"
+              aria-describedby="basic-addon1"/>
+            </InputGroup>
+          </div>
+           <div className="search__info-property">
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">Название</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+              className="mb-3-input"
+              placeholder="+375XXXXXXXX"
+              aria-label="Username"
+              aria-describedby="basic-addon1"/>
+            </InputGroup>
+          </div>
+         
+        </div>
+
         <ListGroup className="search__results">
         <ListGroup.Item className="search__results__result">
             <p className="search__results__result-name">Номер</p>
